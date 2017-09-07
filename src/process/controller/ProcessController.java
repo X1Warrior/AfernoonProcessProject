9074b620cb63fd4265db57aa93a9c188a914e069
X1Warrior <zsto7638@gmail.com>
2017-09-07 12:02:41 -0600

    Initial commit

---
+++ src/process/controller/ProcessController.java
@@ -0,0 +1,10 @@
+package process.controller;
+
+public class ProcessController
+{
+	public void start() 
+	{
+		ProcessController appController = new ProcessController();
+		appController.start();
+	}
+}
